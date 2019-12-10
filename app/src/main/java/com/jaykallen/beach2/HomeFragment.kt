14adad89c822ff8297152838dcaba01d74c21778
Jay Kallen <jkallen@procatdt.local>
2019-12-10 14:30:14 -0500

    Working on Plan UI

--- app/src/main/java/com/jaykallen/beach2/HomeFragment.kt
+++ app/src/main/java/com/jaykallen/beach2/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 // JK 2019-12-09: Attempt to use Room db in Sandbox environment to put into Manage Right later.
 
@@ -23,10 +24,8 @@ class HomeFragment : Fragment() {
 
     private fun setupButtons(view: View) {
         view.findViewById<Button>(R.id.startButton).setOnClickListener {
-
+            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_planFragment)
         }
     }
-
-
 
 }
--- app/src/main/java/com/jaykallen/beach2/PlanFragment.kt
+++ app/src/main/java/com/jaykallen/beach2/PlanFragment.kt
@@ -1,21 +1,52 @@
 package com.jaykallen.beach2
 
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_plan.*
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class PlanFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_plan, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        println("***************** Plan Fragment *******************")
+        setupSeekBar()
+        setupButtons(view!!)
+    }
+
+    private fun setupSeekBar() {
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                makeText.text = progress.toString()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) { }
+            override fun onStopTrackingTouch(seekBar: SeekBar) { }
+        })
+
+    }
+
+    private fun calcTotal() {
+        val barrels: Int = seekBar.progress
+
+    }
+
+    private fun setupButtons(view: View) {
+        view.findViewById<Button>(com.jaykallen.beach2.R.id.startButton).setOnClickListener {
+            Navigation.findNavController(view).navigate(com.jaykallen.beach2.R.id.action_homeFragment_to_planFragment)
+        }
+    }
 
 }
