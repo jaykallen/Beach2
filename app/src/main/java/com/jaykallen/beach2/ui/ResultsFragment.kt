92bfd248e0035cb73a0c17342fcb675fe743ca8a
Jay Kallen <jkallen@procatdt.local>
2019-12-12 11:43:21 -0500

    Fragments Added

---
+++ app/src/main/java/com/jaykallen/beach2/ui/ResultsFragment.kt
@@ -0,0 +1,27 @@
+package com.jaykallen.beach2.ui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.jaykallen.beach2.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+
+class ResultsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_results, container, false)
+    }
+
+
+}
