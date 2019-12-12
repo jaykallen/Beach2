9f871a64ccab68612b81fffe01ab0e43ce78662e
Jay Kallen <jkallen@procatdt.local>
2019-12-12 17:00:41 -0500

    Updated plan

--- app/src/main/java/com/jaykallen/beach2/ui/PlanFragment.kt
+++ app/src/main/java/com/jaykallen/beach2/ui/PlanFragment.kt
@@ -7,15 +7,18 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.jaykallen.beach2.R
 import com.jaykallen.beach2.helper.Helper
+import com.jaykallen.beach2.model.StatusModel
 import kotlinx.android.synthetic.main.fragment_plan.*
 
 
 
 class PlanFragment : Fragment() {
+    var total = 0L
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_plan, container, false)
@@ -39,7 +42,7 @@ class PlanFragment : Fragment() {
         })
         fruitSeek.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                fruitText.text = progress.toString()
+                fruitText.text = (progress * 10).toString()
                 calcTotal()
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) { }
@@ -55,7 +58,7 @@ class PlanFragment : Fragment() {
         })
         priceSeek.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                priceText.text = progress.toString()
+                priceText.text = (progress / 10).toString()
                 calcTotal()
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) { }
@@ -69,17 +72,28 @@ class PlanFragment : Fragment() {
         val fruit: Int = fruitSeek.progress
         val ads: Int = adSeek.progress
         val price: Int = priceSeek.progress
-        costText.text = (barrels + fruit + ads + price + 600).toString()
+        total = barrels * 100 + fruit * 3 + ads * 50L + 600
+        costText.text = total.toString()
     }
 
-    private fun calcBalance() {
-        // todo figure out if costText is less than Balance.  If so then proceed, otherwise stop
+    private fun updateModel() {
+        StatusModel.expenses = total.toFloat()
+        StatusModel.drinks = 100 * makeSeek.progress
+        StatusModel.fruits = 3 * fruitSeek.progress
+        StatusModel.ads = adSeek.progress.toFloat()
+        StatusModel.price = priceSeek.progress.toFloat()
+        Helper.setResults()
     }
 
     private fun setupButtons(view: View) {
         view.findViewById<Button>(R.id.startButton).setOnClickListener {
-            Helper.setResults()
-            Navigation.findNavController(view).navigate(R.id.action_planFragment_to_resultsFragment)
+            if (StatusModel.balance > total) {
+                updateModel()
+                Navigation.findNavController(view)
+                    .navigate(R.id.action_planFragment_to_summaryFragment)
+            } else {
+                Toast.makeText(activity, "Not enough balance for this", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
